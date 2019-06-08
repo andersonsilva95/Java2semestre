755d05b124999843d5c63615e240247b03221e87
andersonsilva95 <anderson_ps12@hotmail.com>
2019-06-07 22:33:42 -0300

    adicionando imports e botões

--- CadastroPessoa/src/view/Exe.java
+++ CadastroPessoa/src/view/Exe.java
@@ -1,5 +1,29 @@
 package view;
 
-public class Exe {
+import java.awt.Container;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Exe extends JFrame {
+	JButton btnCadPF = new JButton("Cadastrar pessoa Física");
+	JButton btnCadPJ = new JButton("Cadastrar pessoa Jurídica");
+	
+	
+	public Executar() {
+		super("Tela de Cadastro");
+		
+		Container paine = this.getContentPane();
+		paine.setLayout(null);
+		
+		btnCadPF.setBounds(100, 100, 100, 100);
+		btnCadPF.addActionListener(new ActionListener()){
+			@Overrider
+			public void actionPerformed(ActionEvent e) {
+				view.PFisica pf = new view.PFisica();
+			}
+		});
+		paine.add(btnCadPF);
+	}
+	
 }
