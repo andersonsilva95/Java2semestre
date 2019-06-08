7f46e751813f03ec8574cabf1cde19cc57d01e40
andersonsilva95 <anderson_ps12@hotmail.com>
2019-06-07 22:18:39 -0300

    07-06

--- CadastroPessoa/src/model/PJuridica.java
+++ CadastroPessoa/src/model/PJuridica.java
@@ -1,5 +1,25 @@
 package model;
 
 public class PJuridica {
-
+	private String razaoSocial;
+	private String nomeFantasia;
+	private double cnpj;
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+	public double getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(double cnpj) {
+		this.cnpj = cnpj;
+	}
 }
--- CadastroPessoa/src/model/Pessoa.java
+++ CadastroPessoa/src/model/Pessoa.java
@@ -1,5 +1,25 @@
 package model;
 
 public class Pessoa {
-
+	private String endereco;
+	private String bairro;
+	private int telefone;
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
 }
